4ed90deb6a927d6313fdc541915670dc1ec00493
Eric Howey <ehowey@gmail.com>
2019-07-17 13:57:04 -0600

    Updated contact to sx pragma

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,80 +1,8 @@
-import React from "react";
-import styled from "styled-components";
-import { graphql, useStaticQuery } from "gatsby";
-import Img from "gatsby-image";
-import FeatherIcon from 'feather-icons-react';
-
-const Section = styled.section`
-  margin: 2rem 0;
-  padding: 2rem 0;
-`;
-
-const ContactContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto;
-  grid-gap: 2rem;
-  max-width: 1024px;
-  margin: 0 auto;
-  padding: 0 1rem;
-`;
-
-const ContactContent = styled.div`
-grid-column: 1 / -1;
-grid-row: 1 / 2;
-z-index: 5;
-@media (min-width: ${props => props.theme.screen.tablet}) {
-grid-column: 2 / 6;
-grid-row: 1 / 2;
-}
-`
-
-const ContactSocial = styled.div`
-display: flex;
-justify-content: space-evenly;
-
-a:hover {
-  opacity: 0.8;
-}
-
-@media (min-width: ${props => props.theme.screen.tablet}) {
-  width: 300px;
-}
-`
-
-const ContactImage = styled(Img)`
-transform: scaleX(-1);
-grid-column: 1 / -1;
-grid-row: 1 / 2;
-opacity: 0.2;
-@media (min-width: ${props => props.theme.screen.tablet}) {
-  grid-column: 1 / 3;
-  grid-row: 1 / 2;
-  opacity: 1;
-}
-`
-
-const Icon = styled(FeatherIcon)`
-padding: 0.5rem;
-border-radius: 50%;
-background-color: ${props => props.theme.color.secondary};
-color: white;
-width: 52px;
-height: 52px;
-`
-
-const ImageOverlay = styled.div`
-z-index: 3;
-display: none;
-background: linear-gradient(90deg, transparent, #ffffff);
-background-size: cover;
-
-@media (min-width: ${props => props.theme.screen.tablet}) {
-  grid-column: 1 / 3;
-  grid-row: 1 / 2;
-  display: block;
-}
-`
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import FeatherIcon from 'feather-icons-react'
 
 const siteSection = () => {
     const data = useStaticQuery(graphql`
@@ -89,21 +17,109 @@ const siteSection = () => {
     }
     `)
     return (
-        <Section id="contact">
-            <ContactContainer>
-                <ContactContent>
-                <h1>I'm ready to listen!</h1>
-                <h3>Inspired idea? New direction? Creating something amazing? Let's talk!</h3>
-                <ContactSocial>
-                <a href="mailto:eric@erichowey.dev"><Icon icon="mail" /></a>
-                <a href="https://github.com/ehowey"><Icon icon="github" /></a>
-                <a href="https://twitter.com/erichoweydev"><Icon icon="twitter" /></a>
-                </ContactSocial>
-                </ContactContent>
-                <ContactImage fluid={data.contactImage1.childImageSharp.fluid} alt="Talking Fish" imgStyle={{ objectFit: 'contain' }}/>
-                <ImageOverlay />
-            </ContactContainer>
-        </Section>
+        <section
+      sx={{
+      my: 5,
+      mx: 0,
+      py: 5
+      px: 0
+      }}
+      id="contact"
+      >
+            <div
+            sx={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
+            gridTemplateRows: "auto",            
+            gridGap: 5,
+            maxWidth: "1024px",
+            margin: "0 auto",
+            py: 0,
+            px: 3
+            }}
+            >
+                <div
+                sx={{
+                gridColumn: ["1 / -1", "2 / 6", null],
+                gridRow: 1 / 2,
+                zIndex: 5
+                }}
+                >
+                  <h1>I'm ready to listen!</h1>
+                  <h3>Inspired idea? New direction? Creating something amazing? Let's talk!</h3>
+                  <div
+                  sx={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  width: ["auto", "300px", null]
+                  }}
+                  >
+                    <a href="mailto:eric@erichowey.dev">
+                    <FeatherIcon
+                    sx={{
+                    padding: 1,
+                    borderRadius: "50%",
+                    backgroundColor: "secondary",
+                    color: "white",
+                    width: "52px",
+                    height: "52px"
+                    }}
+                    icon="mail"
+                     />
+                     </a>
+                    <a href="https://github.com/ehowey">
+                    <FeatherIcon 
+                    <FeatherIcon
+                    sx={{
+                    padding: 1,
+                    borderRadius: "50%",
+                    backgroundColor: "secondary",
+                    color: "white",
+                    width: "52px",
+                    height: "52px"
+                    }}
+                    icon="github"
+                    />
+                    </a>
+                    <a href="https://twitter.com/erichoweydev">
+                    <FeatherIcon 
+                    <FeatherIcon
+                    sx={{
+                    padding: 1,
+                    borderRadius: "50%",
+                    backgroundColor: "secondary",
+                    color: "white",
+                    width: "52px",
+                    height: "52px"
+                    }}
+                    icon="twitter"
+                     />
+                    </a>
+                  </div>
+                </div>
+                <Img
+                sx={{
+                transform: "scaleX(-1)",
+                gridColumn: ["1 / -1", "1 / 3", null],
+                grid-row: 1 / 2,
+                opacity:["0.2", "1", null]
+                }}
+                fluid={data.contactImage1.childImageSharp.fluid} 
+                alt="Talking Fish" 
+                imgStyle={{ objectFit: 'contain' }}
+                />
+                <div 
+                sx={{
+                gridColumn: "1 / 3",
+                gridRow: "1 / 2",
+                zIndex: "3",
+                display: ["none", "block", null],
+                background: "linear-gradient(90deg, transparent, #ffffff)",
+                backgroundSize: "cover"
+                }}
+                />
+            </div>
+        </section>
     )
 }
 
