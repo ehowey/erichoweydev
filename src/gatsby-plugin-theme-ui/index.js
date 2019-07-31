2aa4636a7ee31eb8dfdbc3b1ee25a30f630ec199
Eric Howey <ehowey@gmail.com>
2019-07-30 23:29:30 -0600

    Updated nav components

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -37,7 +37,7 @@ export default merge(baseTheme, {
     maxContentWidth: "100%", // Sets the container size on larger screens, e.g. tablets and laptops
     contentWidth: "100%", // Sets the container width on smaller screens, results in a 5vw margin on the left and right
     headerHeight: "60px", // Sets the height of the header on screens before the first breakpoint, 767px and below by default
-    headerHeightTablet: "160px", // Sets the height of the header after the first breakpoint, 768px to 1023px by default
+    headerHeightTablet: "80px", // Sets the height of the header after the first breakpoint, 768px to 1023px by default
     headerHeightLaptop: "80px", // Sets the height of the header after the second breakpoint, 1024px and up by default
     mainMarginTop: "60px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
     mainMarginTopTablet: "80px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
---
+++ src/gatsby-theme-catalyst-core/components/nav.js
@@ -0,0 +1,56 @@
+/** @jsx jsx */
+import { jsx, useThemeUI } from "theme-ui";
+import NavLinks from "./navLinks";
+import { SocialHeader } from "gatsby-theme-catalyst-core";
+
+const siteNav = props => {
+  const { theme } = useThemeUI();
+  const headOpen = () => {
+    return window.innerHeight - parseInt(theme.sizes.headerHeight) + "px";
+  }; //Used to calculate the height of the nav so it is exactly the height of the window
+  return (
+    <nav
+      sx={{
+        gridColumn: ["1 / -1", "2 / 3", "2 / 3"],
+        gridRow: ["2 / 3", "1 / 2", "1 / 2"],
+        justifySelf: ["center", "end", "end"],
+        alignItems: "center",
+        justifyContent: ["flex-start", "flex-start", "flex-start"],
+        height: [props.open ? headOpen() : 0, "headerHeightTablet", "auto"],
+        mt: [props.open ? 2 : 0, 0, null],
+        display: "flex",
+        flexDirection: ["column", "row", null],
+        width: ["auto", "auto", "auto"]
+      }}
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <NavLinks {...props} />
+      <div
+        sx={{
+          display: [props.open ? "flex" : "none", "flex", null],
+          mr: ["auto", 3, null],
+          ml: "auto",
+          mt: [3, 0, null],
+
+          a: {
+            color: props.open ? "header.iconsOpen" : "header.icons",
+            mr: [3, 2, null],
+            height: theme => theme.sizes.iconsHeader,
+            textDecoration: "none"
+          },
+          "a:last-of-type": {
+            mr: 0
+          },
+          "a:hover": {
+            color: "header.iconsHover"
+          }
+        }}
+      >
+        <SocialHeader />
+      </div>
+    </nav>
+  );
+};
+
+export default siteNav;
