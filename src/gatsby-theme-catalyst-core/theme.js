f98c20c448f837088030162dd8839949e7129027
Eric Howey <ehowey@gmail.com>
2020-03-06 06:52:46 -0700

    Updated margins for bloxkquote

--- src/gatsby-theme-catalyst-core/theme.js
+++ src/gatsby-theme-catalyst-core/theme.js
@@ -150,6 +150,8 @@ export default {
     blockquote: {
       bg: "muted",
       p: 3,
+      my: 4,
+      mx: [1, 2, 4, null, null],
       borderLeft: "5px solid",
       borderColor: "primary",
     },
