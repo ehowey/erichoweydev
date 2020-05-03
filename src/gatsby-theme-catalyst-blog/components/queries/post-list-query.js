191896d68e8d02bd3452d8ad7f3b4ab7f3c56aa5
Eric Howey <ehowey@gmail.com>
2020-05-02 20:49:47 -0600

    Upgrade to v2.0 of catalyst-blog

---
+++ src/gatsby-theme-catalyst-blog/components/queries/post-list-query.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { graphql } from "gatsby"
+import PostList from "../../../components/post-list"
+
+export default ({ data }) => {
+  const { allCatalystPost } = data
+  return <PostList posts={allCatalystPost.nodes} />
+}
+
+export const query = graphql`
+  query {
+    allCatalystPost(
+      sort: { fields: [date, title], order: DESC }
+      limit: 1000
+      filter: { draft: { eq: false } }
+    ) {
+      nodes {
+        id
+        excerpt
+        slug
+        title
+        author
+        authorLink
+        date(formatString: "MMMM DD, YYYY")
+        tags
+        timeToRead
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 1440) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
