2a339fa7799779f145435e7e884d3d345882a49e
Eric Howey <erichowey@Erics-MacBook-Air.local>
2020-09-14 21:04:49 -0600

    Update versions

--- src/gatsby-theme-catalyst-header-top/components/branding/branding-logo.js
+++ src/gatsby-theme-catalyst-header-top/components/branding/branding-logo.js
@@ -1,16 +1,25 @@
 /** @jsx jsx */
 import { jsx, useColorMode } from "theme-ui"
 import Img from "gatsby-image"
+import { Link } from "gatsby"
 import { useContext } from "react"
-import { NavContext } from "gatsby-theme-catalyst-core"
-import { useCatalystConfig } from "gatsby-theme-catalyst-core"
-import { useSiteMetadata } from "gatsby-theme-catalyst-core"
-import LinkWrapper from "gatsby-theme-catalyst-header-top/src/components/branding/branding-link-wrapper"
+import {
+  NavContext,
+  HomeContext,
+  useCatalystConfig,
+  useSiteMetadata,
+} from "gatsby-theme-catalyst-core"
+import { animateScroll as scroll } from "react-scroll"
 
 const SiteLogo = () => {
+  const [isHome] = useContext(HomeContext)
   const { title, logo } = useSiteMetadata()
-  const [isNavOpen] = useContext(NavContext)
-  const { invertSiteLogo } = useCatalystConfig()
+  const [isNavOpen, setIsNavOpen] = useContext(NavContext)
+  const {
+    invertSiteLogo,
+    displaySiteLogo,
+    displaySiteLogoMobile,
+  } = useCatalystConfig()
   const [mode] = useColorMode()
   const isDark = mode === "dark"
   const invertLogo = () => {
@@ -20,34 +29,102 @@ const SiteLogo = () => {
       return "none"
     }
   }
-  return (
-    <LinkWrapper>
-      <Img
+  // Set a value for laptop display of logo
+  const displayLaptop = () => {
+    if (displaySiteLogo) {
+      return "block"
+    } else {
+      return "none"
+    }
+  }
+  // Set a value for mobile display of logo
+  const displayPhone = () => {
+    if (displaySiteLogoMobile) {
+      return "block"
+    } else {
+      return "none"
+    }
+  }
+  if (isHome) {
+    return (
+      <div
         sx={{
-          height: [
-            theme => theme.sizes.logoHeightXS,
-            theme => theme.sizes.logoHeightS,
-            theme => theme.sizes.logoHeightM,
-            theme => theme.sizes.logoHeightL,
-            theme => theme.sizes.logoHeightXL,
-          ],
-          width: [
-            theme => theme.sizes.logoWidthXS,
-            theme => theme.sizes.logoWidthS,
-            theme => theme.sizes.logoWidthM,
-            theme => theme.sizes.logoWidthL,
-            theme => theme.sizes.logoWidthXL,
-          ],
-          mr: 2,
-          filter: isDark ? "invert(1)" : isNavOpen ? invertLogo : "none",
-          variant: "variants.siteLogo",
+          display: [displayPhone, null, displayLaptop, null, null],
+          cursor: "pointer",
         }}
-        fluid={logo}
-        alt={title}
-        imgStyle={{ objectFit: "contain" }}
-      />
-    </LinkWrapper>
-  )
+        onClick={() => {
+          setIsNavOpen(false)
+          scroll.scrollToTop()
+        }}
+        onKeyPress={() => {
+          setIsNavOpen(false)
+          scroll.scrollToTop()
+        }}
+        role="button"
+        tabIndex="0"
+        aria-label="Scroll to top"
+      >
+        <Img
+          sx={{
+            height: [
+              (theme) => theme.sizes.logoHeightXS,
+              (theme) => theme.sizes.logoHeightS,
+              (theme) => theme.sizes.logoHeightM,
+              (theme) => theme.sizes.logoHeightL,
+              (theme) => theme.sizes.logoHeightXL,
+            ],
+            width: [
+              (theme) => theme.sizes.logoWidthXS,
+              (theme) => theme.sizes.logoWidthS,
+              (theme) => theme.sizes.logoWidthM,
+              (theme) => theme.sizes.logoWidthL,
+              (theme) => theme.sizes.logoWidthXL,
+            ],
+            mr: 2,
+            filter: isDark ? "invert(1)" : isNavOpen ? invertLogo : "none",
+            variant: "variants.siteLogo",
+          }}
+          fluid={logo}
+          alt={title}
+          imgStyle={{ objectFit: "contain" }}
+        />
+      </div>
+    )
+  } else {
+    return (
+      <Link
+        to="/"
+        onClick={() => setIsNavOpen(false)}
+        sx={{ textDecoration: "none" }}
+      >
+        <Img
+          sx={{
+            display: [displayPhone, null, displayLaptop, null, null],
+            height: [
+              (theme) => theme.sizes.logoHeightXS,
+              (theme) => theme.sizes.logoHeightS,
+              (theme) => theme.sizes.logoHeightM,
+              (theme) => theme.sizes.logoHeightL,
+              (theme) => theme.sizes.logoHeightXL,
+            ],
+            width: [
+              (theme) => theme.sizes.logoWidthXS,
+              (theme) => theme.sizes.logoWidthS,
+              (theme) => theme.sizes.logoWidthM,
+              (theme) => theme.sizes.logoWidthL,
+              (theme) => theme.sizes.logoWidthXL,
+            ],
+            mr: 2,
+            filter: isDark ? "invert(1)" : isNavOpen ? invertLogo : "none",
+            variant: "variants.siteLogo",
+          }}
+          fluid={logo}
+          alt={title}
+          imgStyle={{ objectFit: "contain" }}
+        />
+      </Link>
+    )
+  }
 }
 
 export default SiteLogo
