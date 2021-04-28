403b9b67be97f8c19b057c1bb3f2d7d82516fb5d
ehowey <ehowey@gmail.com>
2021-04-27 21:27:42 -0600

    Remove filter

--- src/components/header/branding.js
+++ src/components/header/branding.js
@@ -32,7 +32,6 @@ const SiteBranding = () => {
           sx={{
             height: ["40px", "50px", null, "60px", null],
             width: ["61px", "76px", null, "96px", null],
-            filter: isNavOpen ? "invert(1)" : "none",
             variant: "variants.siteLogo",
           }}
           alt={title}
