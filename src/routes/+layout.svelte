4811053cfcc8e4f66139c607a11d0baf44a4f82e
ehowey <ehowey@gmail.com>
2022-10-16 20:59:30 -0600

    Some initial setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<header>Header</header>
+<main class="py-4 px-8">
+	<slot />
+</main>
+<footer>Footer</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Hello and Welcome | Eric Howey - Calgary Based Web Developer</title>
+	<meta
+		name="description"
+		content="Frontend web developer and designer, based in Calgary, Alberta. I work at the intersection of caring and code. Specializing in Javascript, React, Gatsby, NextJS and SANITY."
+	/>
+</svelte:head>
+
+<section class="prose">
+	<h1>Eric Howey</h1>
+	<p>
+		Hello and welcome! My name is Eric Howey and I am a Calgary based web developer who works at the
+		intersection of caring, creativity and code.
+	</p>
+</section>
+
+<style>
+</style>
