cc71a355d9af19eb1b6cd5b59dc8c1b2ae9107b3
ehowey <ehowey@gmail.com>
2022-11-06 19:56:32 -0700

    WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
 import type { HeadFC } from "gatsby";
 
 const HomePage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx(
+        filter: {
+          frontmatter: { type: { eq: "post" }, published: { eq: true } }
+        }
+      ) {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `);
   return (
     <main>
       <h1>Home</h1>
