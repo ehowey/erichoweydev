459c5787e1b07ae9a9ec5f4d061a2156c54b805b
ehowey <ehowey@gmail.com>
2023-01-12 22:42:55 -0700

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
-import image from '@astrojs/image'
-import { remarkCustomFrontmatter } from './src/lib/remarkCustomFrontmatter'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import image from '@astrojs/image';
+import { remarkCustomFrontmatter } from './src/lib/remarkCustomFrontmatter';
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://www.erichowey.dev',
-  integrations: [
-    tailwind(),
-    mdx({
-      remarkPlugins: [remarkCustomFrontmatter],
-    }),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-  ],
-})
+  integrations: [tailwind(), mdx({
+    remarkPlugins: [remarkCustomFrontmatter]
+  }), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap()]
+});
