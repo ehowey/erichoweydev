a52bbd941450898ec5481aa373d544fab2b39140
ehowey <ehowey@gmail.com>
2023-06-09 12:46:42 -0600

    WIP

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -204,182 +204,189 @@ declare module 'astro:content' {
   slug: "10-reasons-to-use-theme-ui-in-your-next-gatsby-project";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "adding-global-script-and-stylesheets-to-gatsby-head/index.mdx": {
 	id: "adding-global-script-and-stylesheets-to-gatsby-head/index.mdx";
   slug: "adding-global-script-and-stylesheets-to-gatsby-head";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "advanced-internationalization-with-nextjs-and-middleware/index.mdx": {
 	id: "advanced-internationalization-with-nextjs-and-middleware/index.mdx";
   slug: "advanced-internationalization-with-nextjs-and-middleware";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "advanced-mapping-with-gatsby-and-react-leaflet/index.mdx": {
 	id: "advanced-mapping-with-gatsby-and-react-leaflet/index.mdx";
   slug: "advanced-mapping-with-gatsby-and-react-leaflet";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "building-a-sub-menu-with-gatsby/index.mdx": {
 	id: "building-a-sub-menu-with-gatsby/index.mdx";
   slug: "building-a-sub-menu-with-gatsby";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "ch-ch-changesets-turn-and-face-the-changelog/index.mdx": {
 	id: "ch-ch-changesets-turn-and-face-the-changelog/index.mdx";
   slug: "ch-ch-changesets-turn-and-face-the-changelog";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "examples-of-using-options-in-gatsby-themes/index.mdx": {
 	id: "examples-of-using-options-in-gatsby-themes/index.mdx";
   slug: "examples-of-using-options-in-gatsby-themes";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "form-submission-using-gatsby-sanity-netlify-react-hook-form/index.mdx": {
 	id: "form-submission-using-gatsby-sanity-netlify-react-hook-form/index.mdx";
   slug: "form-submission-using-gatsby-sanity-netlify-react-hook-form";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "gatsby-theme-catalyst-in-2021/index.mdx": {
 	id: "gatsby-theme-catalyst-in-2021/index.mdx";
   slug: "gatsby-theme-catalyst-in-2021";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "gatsby-themes-pros-and-cons/index.mdx": {
 	id: "gatsby-themes-pros-and-cons/index.mdx";
   slug: "gatsby-themes-pros-and-cons";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "geocoding-with-gatsby-and-sanity/index.mdx": {
 	id: "geocoding-with-gatsby-and-sanity/index.mdx";
   slug: "geocoding-with-gatsby-and-sanity";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "getting-started-with-gatsby-theme-catalyst-writer/index.mdx": {
 	id: "getting-started-with-gatsby-theme-catalyst-writer/index.mdx";
   slug: "getting-started-with-gatsby-theme-catalyst-writer";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "how-to-display-alternate-content-in-dark-mode-using-theme-ui/index.mdx": {
 	id: "how-to-display-alternate-content-in-dark-mode-using-theme-ui/index.mdx";
   slug: "how-to-display-alternate-content-in-dark-mode-using-theme-ui";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "how-to-setup-a-gatsby-themes-monorepo/index.mdx": {
 	id: "how-to-setup-a-gatsby-themes-monorepo/index.mdx";
   slug: "how-to-setup-a-gatsby-themes-monorepo";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "how-to-setup-a-staging-website-with-SANITY/index.mdx": {
 	id: "how-to-setup-a-staging-website-with-SANITY/index.mdx";
   slug: "how-to-setup-a-staging-website-with-sanity";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "how-to-use-theme-ui-variants/index.mdx": {
 	id: "how-to-use-theme-ui-variants/index.mdx";
   slug: "how-to-use-theme-ui-variants";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "introduction-to-catalyst-themes-for-gatsby/index.mdx": {
 	id: "introduction-to-catalyst-themes-for-gatsby/index.mdx";
   slug: "introduction-to-catalyst-themes-for-gatsby";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "load-more-button-and-infinite-scroll-in-gatsby/index.mdx": {
 	id: "load-more-button-and-infinite-scroll-in-gatsby/index.mdx";
   slug: "load-more-button-and-infinite-scroll-in-gatsby";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "notes-from-css-for-js-course/index.mdx": {
 	id: "notes-from-css-for-js-course/index.mdx";
   slug: "notes-from-css-for-js-course";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "reflections-on-redwoodjs/index.mdx": {
 	id: "reflections-on-redwoodjs/index.mdx";
   slug: "reflections-on-redwoodjs";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "should-we-be-thinking-differently-about-imposter-syndrome/index.mdx": {
 	id: "should-we-be-thinking-differently-about-imposter-syndrome/index.mdx";
   slug: "should-we-be-thinking-differently-about-imposter-syndrome";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
+} & { render(): Render[".mdx"] };
+"test-heading-post/index.mdx": {
+	id: "test-heading-post/index.mdx";
+  slug: "test-heading-post";
+  body: string;
+  collection: "writing";
+  data: any
 } & { render(): Render[".mdx"] };
 "understanding-latent-component-shadowing-in-gatsby-themes/index.mdx": {
 	id: "understanding-latent-component-shadowing-in-gatsby-themes/index.mdx";
   slug: "understanding-latent-component-shadowing-in-gatsby-themes";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "using-conditional-fields-in-sanity-for-better-links/index.mdx": {
 	id: "using-conditional-fields-in-sanity-for-better-links/index.mdx";
   slug: "using-conditional-fields-in-sanity-for-better-links";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "using-theme-ui-with-redwoodjs/index.mdx": {
 	id: "using-theme-ui-with-redwoodjs/index.mdx";
   slug: "using-theme-ui-with-redwoodjs";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "using-theme-ui-with-sanity/index.mdx": {
 	id: "using-theme-ui-with-sanity/index.mdx";
   slug: "using-theme-ui-with-sanity";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 "validation-snippets-for-sanity-io/index.mdx": {
 	id: "validation-snippets-for-sanity-io/index.mdx";
   slug: "validation-snippets-for-sanity-io";
   body: string;
   collection: "writing";
-  data: InferEntrySchema<"writing">
+  data: any
 } & { render(): Render[".mdx"] };
 };
 
@@ -391,5 +398,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	type ContentConfig = typeof import("../src/content/config");
+	type ContentConfig = never;
 }
