8631323e07d4e7dc4c16aecfdac17682567622cc
ehowey <ehowey@gmail.com>
2023-10-06 23:00:29 -0600

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import rehypePrettyCode from 'rehype-pretty-code'
+
+const prettyCodeOptions = {
+  theme: 'github-dark',
+}
 
 export default defineConfig({
   site: 'https://www.erichowey.dev',
+  markdown: {
+    extendDefaultPlugins: true,
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+  },
   integrations: [tailwind(), mdx(), sitemap()],
 })
