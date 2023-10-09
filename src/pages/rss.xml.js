01b16f8ec8ce4f9fe9eb01482812941a9ed2ed90
ehowey <ehowey@gmail.com>
2023-10-09 11:02:00 -0600

    TEST: Test excluding unpublished builds

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,13 +2,22 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
 export async function get() {
-  const posts = await getCollection('writing')
+  const blogEntries = await (
+    await getCollection('writing')
+  ).filter((post) => {
+    if (import.meta.env.DEV) {
+      return post
+    }
+    if (post.data.published === true) {
+      return post
+    }
+  })
   return rss({
     title: 'Eric Howey | Writing',
     description:
       'Musing, ramblings and articles about modern frontend web development.',
     site: 'https://www.erichowey.dev',
-    items: posts.map((post) => ({
+    items: blogEntries.map((post) => ({
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
